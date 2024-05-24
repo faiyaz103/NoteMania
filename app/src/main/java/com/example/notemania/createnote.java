2aa6dae618b30bcdb7d237102862ae71c7ba4174
Asib-104 <asib2007104@stud.kuet.ac.bd>
2024-05-25 02:10:35 +0600

    Note Creation Added

--- app/src/main/java/com/example/notemania/createnote.java
+++ app/src/main/java/com/example/notemania/createnote.java
@@ -1,17 +1,43 @@
 package com.example.notemania;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class createnote extends AppCompatActivity {
+    EditText mcreatetitleofnote, mcreatecontentofnote;
+    FloatingActionButton msavenote;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_createnote);
@@ -20,6 +46,64 @@ public class createnote extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+        msavenote=findViewById(R.id.savenote);
+        mcreatecontentofnote=findViewById(R.id.createcontentofnote);
+        mcreatetitleofnote=findViewById(R.id.createtitleofnote);
 
+
+        Toolbar toolbar = findViewById(R.id.toolbarforcreatenote);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+
+         msavenote.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 String title=mcreatetitleofnote.getText().toString();
+                 String content=mcreatecontentofnote.getText().toString();
+                 if(title.isEmpty() || content.isEmpty()){
+                     Toast.makeText(getApplicationContext(),"Both Fields Are Required",Toast.LENGTH_SHORT).show();
+
+                 }
+                 else {
+                     DocumentReference documentReference=firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").document();
+                     Map<String,Object> note=new HashMap<>();
+                     note.put("title",title);
+                     note.put("content",content);
+                     documentReference.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                         @Override
+                         public void onSuccess(Void aVoid) {
+                             Toast.makeText(getApplicationContext(),"Note Created Successfully",Toast.LENGTH_SHORT).show();
+                             startActivity(new Intent(createnote.this,notesActivity.class));
+
+                         }
+                     }).addOnFailureListener(new OnFailureListener() {
+                         @Override
+                         public void onFailure(@NonNull Exception e) {
+                             Toast.makeText(getApplicationContext(),"Failed To Create Note",Toast.LENGTH_SHORT).show();
+
+                         }
+                     });
+                 }
+
+             }
+         });
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
