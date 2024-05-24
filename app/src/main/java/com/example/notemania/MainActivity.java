3191c33356e772982ea5473330a683e9caed459d
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-05-24 16:54:55 +0600

    Buttons are activated

--- app/src/main/java/com/example/notemania/MainActivity.java
+++ app/src/main/java/com/example/notemania/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.notemania;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,58 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
+    private EditText mloginemail,mloginpassword;
+    RelativeLayout mlogin,mgotosignup;
+    private TextView mgotoforgotpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        mloginemail=findViewById(R.id.loginemail);
+        mloginpassword=findViewById(R.id.loginpassword);
+        mlogin=findViewById(R.id.login);
+        mgotoforgotpassword=findViewById(R.id.gotoforgotpassword);
+        mgotosignup=findViewById(R.id.gotosignup);
+
+        mgotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,signup.class));
+            }
+        });
+
+        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,forgotpassword.class));
+            }
+        });
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=mloginemail.getText().toString().trim();
+                String password=mloginpassword.getText().toString().trim();
+
+                if(mail.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"All fields are required",Toast.LENGTH_SHORT).show();
+                }
+                else{
+//                    login the user
+                }
+            }
         });
     }
 }
--- app/src/main/java/com/example/notemania/signup.java
+++ app/src/main/java/com/example/notemania/signup.java
@@ -1,6 +1,12 @@
 package com.example.notemania;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
+import java.util.Objects;
+
 public class signup extends AppCompatActivity {
+
+    private EditText msignupemail,msignuppassword;
+    private RelativeLayout msignup;
+    private TextView mgotologin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        msignupemail=findViewById(R.id.signupemail);
+        msignuppassword=findViewById(R.id.signuppassword);
+        msignup=findViewById(R.id.signup);
+        mgotologin=findViewById(R.id.gotologin);
+
+        mgotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(signup.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        msignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=msignupemail.getText().toString().trim();
+                String password=msignuppassword.getText().toString().trim();
+
+                if(mail.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"All fields are required",Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length()<7){
+                    Toast.makeText(getApplicationContext(),"Password should be at least 7 characters",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //Register the user to firebase
+                }
+            }
         });
     }
 }
