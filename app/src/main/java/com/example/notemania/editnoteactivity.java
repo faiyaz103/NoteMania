8f4207dbafe0720cd8f47367c82ce647df075f24
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-05-27 21:01:55 +0600

    Edit Note and View Note functionality Updated

--- app/src/main/java/com/example/notemania/editnoteactivity.java
+++ app/src/main/java/com/example/notemania/editnoteactivity.java
@@ -1,6 +1,13 @@
 package com.example.notemania;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +15,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class editnoteactivity extends AppCompatActivity {
 
+    Intent data;
+    EditText medittitleofnote,meditcontentofnote;
+    FloatingActionButton msaveeditnote;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +44,55 @@ public class editnoteactivity extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+        medittitleofnote=findViewById(R.id.edittitleofnote);
+        meditcontentofnote=findViewById(R.id.editcontentofnote);
+        msaveeditnote=findViewById(R.id.saveeditnote);
+        data=getIntent();
+
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+
+        Toolbar toolbar=findViewById(R.id.toolbarforeditnote);
+
+        setSupportActionBar(toolbar);//Lookout
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//lookout
+
+        msaveeditnote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(),"Saved Button Clicked",Toast.LENGTH_SHORT).show();
+
+                String newtitle=medittitleofnote.getText().toString();
+                String newcontent=meditcontentofnote.getText().toString();
+
+                if(newtitle.isEmpty() || newcontent.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Something is empty",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else{
+                    DocumentReference documentReference=firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").document(data.getStringExtra("noteId"));
+                    Map<String,Object> note=new HashMap<>();
+                    note.put("title",newtitle);
+                    note.put("content",newcontent);
+                    documentReference.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(getApplicationContext(),"Note is updated",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(editnoteactivity.this,notesActivity.class));
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(),"Failed to update",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        String notetitle=data.getStringExtra("title");
+        String notecontent=data.getStringExtra("content");
+        meditcontentofnote.setText(notecontent);
+        medittitleofnote.setText(notetitle);
     }
 }
